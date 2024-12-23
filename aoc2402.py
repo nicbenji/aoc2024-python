758369c73b2e81002a90db7de7e77b5724497cdb
nicbenji <172269214+nicbenji@users.noreply.github.com>
2024-12-23 16:26:58 +0100

    day 2 solution overhaul

--- aoc2402.py
+++ aoc2402.py
@@ -3,13 +3,13 @@ def checkReport(report):
     isDecreasing = False
     problemIndex = -1
 
+    if len(report) <= 1:
+        return problemIndex
     for j in range(len(report) - 1):
 
         diff = report[j + 1] - report[j]
 
         if abs(diff) < 1 or abs(diff) > 3: 
-            isDecreasing = False
-            isIncreasing = False
             problemIndex = j
             break
         if diff > 0:
@@ -22,25 +22,25 @@ def checkReport(report):
 
     return problemIndex
 
-file = open("aoc2402.txt")
+with open("aoc2402.txt") as file:
 
-count = 0
-for i, line in enumerate(file.readlines()):
-    report = list(map(int, line.split(" ")))
-    print(report)
-    problemIndex = checkReport(report)
+    count = 0
+    for line in file:
+        report = list(map(int, line.split(" ")))
+        print(report)
+        problemIndex = checkReport(report)
 
-    print("Index:", problemIndex)
-    if problemIndex < 0:
-        count += 1
-    else:
-        for j in [problemIndex - 1, problemIndex, problemIndex + 1]:
-            reportCopy = report.copy()
-            reportCopy.pop(j)
+        print("Index:", problemIndex)
+        if problemIndex < 0:
+            count += 1
+        else:
+            for j in [problemIndex - 1, problemIndex, problemIndex + 1]:
+                reportCopy = report.copy()
+                reportCopy.pop(j)
 
-            if checkReport(reportCopy) < 0:
-                count += 1
-                break
+                if checkReport(reportCopy) < 0:
+                    count += 1
+                    break
 
 
 print(count)
